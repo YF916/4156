e82323957df6203db41abc516ac5a5e236fab67b
YF916 <yifanli0916@gmail.com>
2023-11-27 10:41:14 -0500

    Dispatch Controller Unit Testing and CI

--- src/test/java/com/example/demotest/DispatchControllerTest.java
+++ src/test/java/com/example/demotest/DispatchControllerTest.java
@@ -2,6 +2,7 @@ package com.example.demotest;
 
 
 import com.example.demotest.controller.DispatchController;
+import com.example.demotest.exceptions.RequestAlreadyHandledException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.example.demotest.controller.DispatchHistoryController;
@@ -25,36 +27,182 @@ import static org.mockito.Mockito.*;
 
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 @ExtendWith(MockitoExtension.class)
 public class DispatchControllerTest {
 
-    @InjectMocks
-    private DispatchController dispatchController;
-
     @Mock
     private ResponderRepository responderRepository;
 
-    @Captor
-    private ArgumentCaptor<Responder> responderCaptor;
+    @Mock
+    private DispatchHistoryRepository dispatchHistoryRepository;
 
-    @Captor
-    private ArgumentCaptor<Double> minLatCaptor;
+    @Mock
+    private UserRepository userRepository;
 
-    @Captor
-    private ArgumentCaptor<Double> maxLatCaptor;
+    @Mock
+    private UserDetails userDetails;
 
-    @Captor
-    private ArgumentCaptor<Double> minLonCaptor;
 
-    @Captor
-    private ArgumentCaptor<Double> maxLonCaptor;
+    @InjectMocks
+    private DispatchController dispatchController;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
     }
+
+    // Write individual test methods here for each functionality
+    @Test
+    public void testGetAllRequests() {
+        when(dispatchHistoryRepository.findAllByStatusOrderByEmergencyLevelAsc("pending")).thenReturn(new ArrayList<>());
+
+        Iterable<DispatchHistory> result = dispatchController.getAllRequests();
+
+        verify(dispatchHistoryRepository).findAllByStatusOrderByEmergencyLevelAsc("pending");
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testGetRequestByRadius() {
+        when(dispatchHistoryRepository.findAllByLatitudeBetweenAndLongitudeBetweenAndStatusEqualsOrderByEmergencyLevel(anyDouble(), anyDouble(), anyDouble(), anyDouble(), eq("pending")))
+                .thenReturn(new ArrayList<>());
+
+        Iterable<DispatchHistory> result = dispatchController.getRequestByRadius(1.0, 1.0, 0.5);
+
+        verify(dispatchHistoryRepository).findAllByLatitudeBetweenAndLongitudeBetweenAndStatusEqualsOrderByEmergencyLevel(anyDouble(), anyDouble(), anyDouble(), anyDouble(), eq("pending"));
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testDispatchResponder_Success() {
+        when(userDetails.getUsername()).thenReturn("responderUsername");
+        when(responderRepository.getReferenceById("responderUsername")).thenReturn(new Responder());
+        DispatchHistory history = new DispatchHistory();
+        history.setStatus("pending");
+        when(dispatchHistoryRepository.getReferenceById(1)).thenReturn(history);
+
+        String result = dispatchController.dispatchResponder(userDetails, 1);
+
+        assertEquals("Accepted", result);
+        verify(dispatchHistoryRepository).save(history);
+    }
+
+    @Test
+    public void testDispatchResponder_AlreadyHandled() {
+        when(userDetails.getUsername()).thenReturn("responderUsername");
+        when(responderRepository.getReferenceById("responderUsername")).thenReturn(new Responder());
+        DispatchHistory history = new DispatchHistory();
+        history.setStatus("dispatched");
+        when(dispatchHistoryRepository.getReferenceById(1)).thenReturn(history);
+
+        assertThrows(RequestAlreadyHandledException.class, () -> {
+            dispatchController.dispatchResponder(userDetails, 1);
+        });
+    }
+
+    private DispatchHistory createHistoryWithRating(Responder responder, Double rating) {
+        DispatchHistory history = new DispatchHistory();
+        history.setResponder(responder);
+        history.setRating(rating);
+        return history;
+    }
+
+    private DispatchHistory createHistoryWithResponder(Responder responder) {
+        DispatchHistory history = new DispatchHistory();
+        history.setResponder(responder);
+        return history;
+    }
+
+
+    @Test
+    public void testGetRateRecommend_WithRatings() {
+        List<DispatchHistory> histories = new ArrayList<>();
+        Responder highestRatedResponder = new Responder(); // Assuming this is the correct way to instantiate a Responder
+        highestRatedResponder.setName("responder2"); // Assuming a setter method for ID
+        histories.add(createHistoryWithRating(new Responder(), 3.0));
+        histories.add(createHistoryWithRating(highestRatedResponder, 4.0));
+        when(userRepository.getReferenceById("userName")).thenReturn(new User());
+        when(dispatchHistoryRepository.findByCaller(any(User.class))).thenReturn(histories);
+
+        Responder result = dispatchController.getRateRecommend("userName");
+
+        assertEquals("responder2", result.getName()); // Assuming getId exists
+    }
+
+    @Test
+    public void testGetRateRecommend_NoRatings() {
+        when(userRepository.getReferenceById("userName")).thenReturn(new User());
+        when(dispatchHistoryRepository.findByCaller(any(User.class))).thenReturn(new ArrayList<>());
+
+        Responder result = dispatchController.getRateRecommend("userName");
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetFreqRecommend_VariousFrequencies() {
+        List<DispatchHistory> histories = new ArrayList<>();
+        Responder mostFrequentResponder = new Responder(); // Assuming this is the correct way to instantiate a Responder
+        mostFrequentResponder.setName("responder1"); // Assuming a setter method for ID
+        histories.add(createHistoryWithResponder(mostFrequentResponder));
+        histories.add(createHistoryWithResponder(new Responder()));
+        histories.add(createHistoryWithResponder(mostFrequentResponder));
+        when(userRepository.getReferenceById("userName")).thenReturn(new User());
+        when(dispatchHistoryRepository.findByCaller(any(User.class))).thenReturn(histories);
+
+        Responder result = dispatchController.getFreqRecommend("userName");
+
+        assertEquals("responder1", result.getName()); // Assuming getId exists
+    }
+
+    @Test
+    public void testGetFreqRecommend_NoHistory() {
+        when(userRepository.getReferenceById("userName")).thenReturn(new User());
+        when(dispatchHistoryRepository.findByCaller(any(User.class))).thenReturn(new ArrayList<>());
+
+        Responder result = dispatchController.getFreqRecommend("userName");
+
+        assertNull(result);
+    }
+
+
+
+
+
+
+
+
+
+
+
+//    @InjectMocks
+//    private DispatchController dispatchController;
+//
+//    @Mock
+//    private ResponderRepository responderRepository;
+//
+//    @Captor
+//    private ArgumentCaptor<Responder> responderCaptor;
+//
+//    @Captor
+//    private ArgumentCaptor<Double> minLatCaptor;
+//
+//    @Captor
+//    private ArgumentCaptor<Double> maxLatCaptor;
+//
+//    @Captor
+//    private ArgumentCaptor<Double> minLonCaptor;
+//
+//    @Captor
+//    private ArgumentCaptor<Double> maxLonCaptor;
+//
+//    @BeforeEach
+//    public void setup() {
+//        MockitoAnnotations.openMocks(this);
+//    }
 
     /*@Test
     public void testAddNewResponder_Success() {
