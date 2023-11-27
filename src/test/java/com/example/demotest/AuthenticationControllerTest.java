769aa2f4d1c08ddde0b8a1d0e65d590f5709a30f
YF916 <yifanli0916@gmail.com>
2023-11-27 10:17:52 -0500

    Register Controller Unit Testing and CI

--- src/test/java/com/example/demotest/AuthenticationControllerTest.java
+++ src/test/java/com/example/demotest/AuthenticationControllerTest.java
@@ -1,14 +1,12 @@
 package com.example.demotest;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import com.example.demotest.service.AuthenticationService;
 
 import com.example.demotest.controller.AuthenticationController;
 import com.example.demotest.exceptions.InvalidRequestException;
-import com.example.demotest.service.AuthenticationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,7 +14,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -24,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
-public class ControllerTest {
+public class AuthenticationControllerTest {
 
     private MockMvc mockMvc;
 
@@ -40,6 +37,7 @@ public class ControllerTest {
     public void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(authenticationController).build();
     }
+
 
     @Test
     public void testAuthenticateUser_Success() {
